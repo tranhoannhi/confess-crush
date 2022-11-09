73c3c878714a567d92e70936289cc722d511c8b7
tranhoannhi <tranhoannhi0310@gmail.com>
2022-11-09 17:28:18 +0700

    init

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "San love Nhi Forever",
+    introTitle: 'Trần Hoàn Nhi',
+    introDesc: `They say as long as at least one person cares for you, life isn’t a waste. So when things go terribly wrong, and you feel like giving up, please remember you still got me`,
+    btnIntro: 'Continue',
+    title: 'I looked at your face… my heart jumped all over the place.',
+    desc: 'Do you really love San? ',
+    btnYes: 'I love so much <33',
+    btnNo: 'No, I do not love you :3',
+    question: 'There are 7 billion people in the world. Why do you love San?',
+    btnReply: 'Sent to San <3',
+    reply: 'Nhi love San every universe <33333333',
+    mess: 'Really, I love you too 🥰. Meeting you is the best thing that ever happened to me. 😘😘',
+    messDesc: 'Will you come to our dinner tonight?',
+    btnAccept: 'Okiiiii <3',
+    messLink: 'https://www.facebook.com/DangSan1997'
+}
